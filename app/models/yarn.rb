420e0cdb370f5c7de5258f3a52a29bf1606af2b2
taylorsieling <taylorasieling@gmail.com>
2020-11-30 11:32:43 -0600

    created tables and associations

--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -1,2 +1,3 @@
 class Yarn < ActiveRecord::Base
+    belongs_to :project
 end
--- db/migrate/20201130170555_create_yarns.rb
+++ db/migrate/20201130170555_create_yarns.rb
@@ -1,7 +1,10 @@
 class CreateYarns < ActiveRecord::Migration
   def change
     create_table :yarns do |t|
-
+      t.string :name
+      t.integer :weight
+      t.string :color
+      t.integer :quantity
       t.timestamps null: false
     end
   end
