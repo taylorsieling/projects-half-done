d706e64fa1404cfc02825234137010bee105380a
taylorsieling <taylorasieling@gmail.com>
2020-12-04 10:59:18 -0600

    added brand to views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201202034644) do
+ActiveRecord::Schema.define(version: 20201204163639) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20201202034644) do
     t.integer  "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "brand"
   end
 
 end
