7a0e0763b1bf1890dfe423133397a7f17bd3b66d
taylorsieling <taylorasieling@gmail.com>
2020-12-08 16:45:32 -0600

    stylized edit pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_many :projects
     has_many :yarns
     has_secure_password
+
+    validates :username, presence: true
+    validates :password_digest, :email, presence: true
+    validates :email, uniqueness: true
 end
