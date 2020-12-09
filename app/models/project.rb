ed1a31e44f9a7730e11cacd07d116f5551be82b1
taylorsieling <taylorasieling@gmail.com>
2020-12-08 20:24:19 -0600

    added yarn and project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
     has_many :yarns
     belongs_to :user
+
+    validates :name, :category, :knit_time, :status, presence: true
 end
--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -1,4 +1,6 @@
 class Yarn < ActiveRecord::Base
     belongs_to :project
     belongs_to :user
+
+    validates :brand, :name, :weight, :color, :quantity, presence: true
 end
